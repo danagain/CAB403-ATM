73fe0f21320149279704d90c1bc9287b226ae61a
Daniel Huffer <danielhuffer@live.com>
2016-10-08 14:50:03 +1000

    project

---
+++ client.c
@@ -0,0 +1,373 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+
+char username[30];
+char client_pin[15];
+char input[15];
+char ans[15];
+char withdraw[15];
+char deposit[15];
+char outServer[64];
+char* inServer;
+int transCount;
+int closing;
+bool login = false;
+char* temp;
+bool selection = false;
+char buf[1000];
+
+
+
+void introMenu(char* name, char* surname, char *clientNumber){
+printf("\n\nWelcome to the ATM System");
+printf("\n\nYou are currently logged in as %s %s \n", name, surname);
+printf("Client Number - %s" , clientNumber);
+}
+
+char* selectionMenu(){
+printf("\n\nPlease enter a selection");
+printf("\n<1> Account Balance");
+printf("\n<2> Withdrawal");
+printf("\n<3> Deposit");
+printf("\n<4> Transfer");
+printf("\n<5> Transaction Listing");
+printf("\n<6> EXIT");
+printf("\n\nSelect option 1-6 -->");
+//printf("\nOpening Balance : %s", clientOpen);
+//printf("\nClosing Balance : %s", clientClose);
+scanf("%s",input);
+return input;
+}
+
+
+
+char *Receive_Array_char_Data(int socket_identifier, int size) {
+    int number_of_bytes, i=0;
+    char names;
+
+	char *resultss = malloc(sizeof(char)*size);
+	for (i=0; i < size; i++) {
+		if ((number_of_bytes=recv(socket_identifier, &names, sizeof(char), 0))
+		         == -1) {
+			perror("recv");
+			exit(EXIT_FAILURE);
+		}
+		resultss[i] = names;
+	}
+	return resultss;
+}
+
+
+	#define MAXDATASIZE 100 /* max number of bytes we can get at once */
+
+	#define ARRAY_SIZE 30
+void interface(){
+  printf("===========================================================\n\n");
+  printf("Welcome to the Online ATM System \n\n");
+  printf("===========================================================\n\n");
+  printf("\n\n\nYou are required to logn with your registered Username and PIN\n");
+	printf("\nPlease enter your Username --> ");
+  username[15] = scanf("%s", username);
+  printf("\nPlease enter your pin --> ");
+  client_pin[15] = scanf("\n%s", client_pin);
+
+}
+
+char* whatAccounts(char* ac1, char* ac2, char* ac3){
+
+printf("\n\nSelect account type");
+//ACCOUNT 1
+if(atoi(ac1) > 0 && atoi(ac1) % 11 == 0){
+printf("\n<1> Savings Account");
+}
+if(atoi(ac1) > 0 && atoi(ac1) % 12 == 0){
+printf("\n<1> Loan Account");
+}
+if(atoi(ac1) > 0 && atoi(ac1) % 13 == 0){
+printf("\n<1> Credit Card Account");
+}
+//ACCOUNT 2
+if(atoi(ac2) > 0 && atoi(ac2) % 11 == 0){
+printf("\n<2> Savings Account");
+}
+if(atoi(ac2) > 0 && atoi(ac2) % 12 == 0){
+printf("\n<2> Loan Account");
+}
+if(atoi(ac2) > 0 && atoi(ac2) % 13 == 0){
+printf("\n<2> Credit Card Account");
+}
+//ACCOUNT 3
+if(atoi(ac3) > 0 && atoi(ac3) % 11 == 0){
+printf("\n<3> Savings Account");
+}
+if(atoi(ac3) > 0 && atoi(ac3) % 12 == 0){
+printf("\n<3> Loan Account");
+}
+if(atoi(ac3) > 0 && atoi(ac3) % 13 == 0){
+printf("\n<3> Credit Card Account");
+}
+printf("\nEnter your selection (E/e to exit) - ");
+scanf("%s",ans);
+return ans;
+}
+
+
+
+void clrscr()
+{
+    system("@cls||clear");
+}
+
+void Send_Username(int socket_id, char *myArray, char *myArray2) {
+	int i=0;
+	char names;
+	for (i = 0; i < 15; i++) {
+		names = myArray[i];
+		send(socket_id, &names, sizeof(char), 0);
+	}
+	for (i = 0; i < 13; i++) {
+		names = myArray2[i];
+		send(socket_id, &names, sizeof(char), 0);
+	}
+	//username length for match purposes
+	for (i = 0; i < 1; i++) {
+		names = strlen(myArray) + '0';
+		send(socket_id, &names, sizeof(char), 0);
+	}
+		//username length for match purposes
+	for (i = 0; i < 1; i++) {
+		names = strlen(myArray2) + '0';
+		send(socket_id, &names, sizeof(char), 0);
+	}
+}
+
+void Send_Trans(int socket_id, char *myArray) {
+	int i=0;
+	char names;
+	for (i = 0; i < transCount; i++) {
+		names = myArray[i];
+		send(socket_id, &names, sizeof(char), 0);
+	}
+	
+}
+
+
+int main(int argc, char *argv[]) {
+	int sockfd, numbytes, i=0;
+	char buf[MAXDATASIZE];
+	struct hostent *he;
+	struct sockaddr_in their_addr; /* connector's address information */
+
+	if (argc != 3) {
+		fprintf(stderr,"usage: client_hostname port_number\n");
+		exit(1);
+	}
+
+	if ((he=gethostbyname(argv[1])) == NULL) {  /* get the host info */
+		herror("gethostbyname");
+		exit(1);
+	}
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket");
+		exit(1);
+	}
+
+
+	their_addr.sin_family = AF_INET;      /* host byte order */
+	their_addr.sin_port = htons(atoi(argv[2]));    /* short, network byte order */
+	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+	bzero(&(their_addr.sin_zero), 8);     /* zero the rest of the struct */
+
+
+	if (connect(sockfd, (struct sockaddr *)&their_addr, \
+	sizeof(struct sockaddr)) == -1) {
+		perror("connect");
+		exit(1);
+	}
+	clrscr();
+	interface(); // create user interface of an ATM , take username and
+//			pin number and send to server for authentication
+
+
+	//Send_Username(sockfd, username, client_pin);
+	//char* buff = Receive_Array_char_Data(sockfd, 1024);
+	//if(buff[0] == 'E' && buff[1] == 'R' && buff[2] == 'R'){
+	//printf("Incorrect Username and/or Password");
+	//close(sockfd);
+	//return 0;
+	//}
+
+char message[1000] , server_reply[2000], fromBuf[120];
+
+
+Send_Username(sockfd,username, client_pin);
+recv(sockfd, fromBuf, 120, 0);
+
+
+
+char clientName[14];
+char clientSurname[14];
+char clientNum[14];
+char clientAc1[14];
+char clientAc2[14];
+char clientAc3[14];
+char clientOpen[14];
+char clientClose[14];
+
+//Client Name
+for(int i = 0; i < fromBuf[14]; i++){
+clientName[i] = fromBuf[i];
+}
+//Client Surname
+for(int i = 0; i < fromBuf[15+15]; i++){
+clientSurname[i] = fromBuf[i+15];
+}
+//client Number
+for(int i = 0; i < fromBuf[15+15+15]; i++){
+clientNum[i] = fromBuf[i+30];
+}
+//Account number 1
+for(int i = 0; i < fromBuf[15+15+15+15]; i++){
+clientAc1[i] = fromBuf[i+45];
+}
+//Account number 2
+for(int i = 0; i < fromBuf[15+15+15+15+15]; i++){
+clientAc2[i] = fromBuf[i+60];
+}
+//Account number 3
+for(int i = 0; i < fromBuf[15+15+15+15+15+15]; i++){
+clientAc3[i] = fromBuf[i+75];
+}
+//Clients Opening Balance
+for(int i = 0; i < fromBuf[15+15+15+15+15+15+15]; i++){
+clientOpen[i] = fromBuf[i+90];
+}
+//Clients Opening Balance
+for(int i = 0; i < fromBuf[15+15+15+15+15+15+15+15]; i++){
+clientClose[i] = fromBuf[i+105];
+}
+
+closing = atoi(clientClose);
+
+	if(strcmp(fromBuf,"ERROR Username and/or pin was INCORRECT") == 0){
+	    close(sockfd);
+	printf("Goodbye");
+	return 0;
+	exit(1);
+}
+
+login = true;
+clrscr();
+ while(1)
+    {	
+	if(selection == true){
+	selectionMenu();
+}
+//        printf("Enter message : ");
+       // fgets(message ,1000,stdin);
+	//puts(message);
+	while(atoi(&input[0]) == 1) { 
+	whatAccounts(clientAc1, clientAc2, clientAc3);
+	if(atoi(&ans[0]) == 1){
+
+printf(" Savings Balance Selected - > Send savings Balance variable to server \n");
+write(sockfd , "BALANCE SAVINGS" , strlen("BALANCE SAVINGS")+1);
+        //Receive a reply from the server
+        if( recv(sockfd , server_reply , 2000 , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }   
+
+       // puts("Server reply :");
+	printf("===================================================================\n\n\n");
+        puts(server_reply);
+printf("\n\n\n===================================================================\n\n\n");
+	selection = true;
+break;
+}        
+}
+
+while(atoi(&input[0]) == 2) {
+whatAccounts(clientAc1, clientAc2, clientAc3);
+	if(atoi(&ans[0]) == 1){
+	char* withdrawal;
+	printf(" Savings Balance Selected - > Send savings Withdrawal variable to server \n");
+	printf("How much would you like to withdraw : ");
+	//fgets(message,1000,stdin);
+	scanf("%s" ,message);
+	//fgets(message,1000,stdin);
+	//sscanf(message, "%s" , withdrawal);
+	sprintf(buf, "WITHDRAWSAV %s", message);
+	write(sockfd , buf , strlen(buf));
+	        //Receive a reply from the server
+        if( recv(sockfd , server_reply , 2000 , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }   
+       // puts("Server reply :");
+	printf("===================================================================\n\n\n");
+        puts(server_reply);
+printf("\n\n\n===================================================================\n\n\n");
+	selection = true;
+break;
+
+}
+}
+
+ 
+       // Send some data
+      //  if( send(sockfd , message , strlen(message)+1 , 0) < 0)
+        //{
+          //  puts("Send failed");
+            //return 1;
+        //}
+         
+
+         
+
+        //Receive a reply from the server
+       // if( recv(sockfd , server_reply , 2000 , 0) < 0)
+        //{
+         //   puts("recv failed");
+           // break;
+        //}
+	
+
+         
+       // puts("Server reply :");
+       // puts(server_reply);
+
+	//if(atoi(&ans[0]) == 1){
+	//selectionMenu();
+//}
+
+
+	if(login == true){
+	clrscr();
+	introMenu(clientName, clientSurname, clientNum);
+	printf("\n");
+	selectionMenu();
+}
+	login = false;
+	//memset(&message[0], 0, sizeof(message));
+    }
+     
+    close(sockfd);
+    return 0;
+}
+
+
+
+S
---
+++ server.c
@@ -0,0 +1,495 @@
+/*
+*  Materials downloaded from the web. See relevant web sites listed on OLT
+*  Collected and modified for teaching purpose only by Jinglan Zhang, Aug. 2006
+*/
+
+
+#include <arpa/inet.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <sys/wait.h> 
+#include <unistd.h>
+#include <errno.h>
+#include <stdbool.h>
+
+FILE * clientDetails;
+FILE * toAuthenticate;
+FILE * accountsFile;
+char inClientBuffer[64];
+char outClientBuffer[1024];
+char usernameSaved[15];
+int pin;
+int clientNum;
+bool login = false;
+char detailsname[15];
+char detailssurname[15];
+int detailsaccount1;
+int detailsaccount2;
+int detailsaccount3;
+int detailsclientnum;
+int accountNumber;
+float openingBalance;
+float closingBalance;
+int saveTracker;
+          char onlineCnum[14];
+          char onlineAc1[14];
+          char onlineAc2[14];
+          char onlineAc3[14];
+          char onlineName[14];
+          char onlineSurname[14];
+	int usersClientNum;
+
+					char onlineOpen[14];
+					char onlineClose[14];
+
+  //AUTH STRUCT
+  struct Clients {
+    char username[15];
+    int pinNumber;
+    int clientNumber;
+  };
+  struct Clients clients[14];
+
+  //CLIENT INFO STRUCT
+  struct ClientsInfo {
+    char name[15];
+    char surname[15];
+    int clientnum;
+		struct Figs{
+		float open;
+		float close;
+		}figs;
+    int account1;
+    int account2;
+    int account3;
+  };
+  struct ClientsInfo clientsinfo[10];
+
+
+	struct Accounts{
+		int accNum;
+		float openBal;
+		float closeBal;
+	};struct Accounts accounts[24];
+
+
+	#define ARRAY_SIZE 30  /* Size of array to receive */
+
+	#define BACKLOG 10     /* how many pending connections queue will hold */
+
+	#define RETURNED_ERROR -1
+
+void fillClients(){
+//Variables
+toAuthenticate = fopen("Authentication.txt", "r");
+int cnt = 0;
+char x;
+ char str[100];
+//Clearing first line
+  do {
+    x = fgetc(toAuthenticate);
+  } while (x != '\n');
+  //SCANNING AUTH.txt STORING ALL DATA IN STRUCTS
+  while (fgets(str, 100, toAuthenticate)) {
+		cnt++;
+    sscanf(str, "%s %d %d", usernameSaved, &pin, &clientNum);
+    if (feof(toAuthenticate))
+      break;
+    //Attempting to use my array of structs to loop and store all the pins of every user
+    clients[cnt].pinNumber = pin;
+    //storing every clients client number
+    clients[cnt].clientNumber = clientNum;
+    //storing client usernames
+    for (int i = 0; i < 15; i++) {
+      clients[cnt].username[i] = usernameSaved[i];
+    }
+    printf("clients user %s , clients pin %i\n", clients[cnt].username, clients[cnt].pinNumber);
+  }
+}
+
+void fillClientsInfo(){
+  char str[100];
+  char c;
+  int chk;
+  int cnt2 = 0;
+clientDetails = fopen("Client_Details.txt", "r");
+do{
+    c = fgetc(clientDetails);
+  } while (c != '\n');
+
+  while (fgets(str, 100, clientDetails)) {
+
+    /* writing content to stdout */
+    //puts(str);
+    chk = sscanf(str, "%s %s %d %d, %d, %d", detailsname, detailssurname, & detailsclientnum, & detailsaccount1, & detailsaccount2, & detailsaccount3);
+	if(chk < 4 ){
+		break;	
+		}
+    if (chk == 4) {
+      for (int i = 0; i < 15; i++) {
+        clientsinfo[cnt2].name[i] = detailsname[i];
+        clientsinfo[cnt2].surname[i] = detailssurname[i];
+      }
+      clientsinfo[cnt2].clientnum = detailsclientnum;
+      clientsinfo[cnt2].account1 = detailsaccount1;
+      clientsinfo[cnt2].account2 = 0;
+      clientsinfo[cnt2].account3 = 0;
+    }
+    if (chk == 5) {
+      for (int i = 0; i < 15; i++) {
+        clientsinfo[cnt2].name[i] = detailsname[i];
+        clientsinfo[cnt2].surname[i] = detailssurname[i];
+      }
+      clientsinfo[cnt2].clientnum = detailsclientnum;
+      clientsinfo[cnt2].account1 = detailsaccount1;
+      clientsinfo[cnt2].account2 = detailsaccount2;
+      clientsinfo[cnt2].account3 = 0;
+    }
+    if (chk == 6) {
+      for (int i = 0; i < 15; i++) {
+        clientsinfo[cnt2].name[i] = detailsname[i];
+        clientsinfo[cnt2].surname[i] = detailssurname[i];
+      }
+      clientsinfo[cnt2].clientnum = detailsclientnum;
+      clientsinfo[cnt2].account1 = detailsaccount1;
+      clientsinfo[cnt2].account2 = detailsaccount2;
+      clientsinfo[cnt2].account3 = detailsaccount3;
+    }
+    if (clientsinfo[cnt2].clientnum == clientsinfo[cnt2 - 1].clientnum) {
+      break;
+    }
+		printf("\n\n%s %s %d %d %d %d\n\n", detailsname, detailssurname, detailsclientnum, detailsaccount1, detailsaccount2, detailsaccount3);
+    cnt2++;
+  }
+}
+
+void fillAccounts(){
+int cnt3 = 0;
+char x;
+accountsFile = fopen("Accounts.txt", "r");
+	char str3[100];
+	//skipping first line of accounts file
+do{
+    x = fgetc(accountsFile);
+ }while (x != '\n');
+
+	while (fgets(str3, 100, accountsFile)) {
+	sscanf(str3,"%i %f %f" , &accountNumber, &openingBalance, &closingBalance);
+	accounts[cnt3].accNum = accountNumber;
+	accounts[cnt3].openBal = openingBalance;
+	accounts[cnt3].closeBal = closingBalance;
+    if (accounts[cnt3].accNum == accounts[cnt3-1].accNum){
+      break;}
+	cnt3++;
+	}
+for(int i = 0; i < 24; i++){
+printf("\n acc = %i open = %f close = %f \n" , accounts[i].accNum, accounts[i].openBal, accounts[i].closeBal);
+
+}
+}
+
+char * Receive_Array_char_Data(int socket_identifier, int size) {
+  int number_of_bytes, i = 0;
+  char names;
+
+  char * resultss = malloc(sizeof(char) * size);
+  for (i = 0; i < size; i++) {
+    if ((number_of_bytes = recv(socket_identifier, &names, sizeof(char), 0)) == RETURNED_ERROR) {
+      perror("recv");
+      exit(EXIT_FAILURE);
+    }
+    resultss[i] = names;
+  }
+  return resultss;
+}
+
+void sendStrings(int socket_id, char * theArray, int length) {
+  int i = 0;
+  char characters;
+  for (i = 0; i < length; i++) {
+    characters = theArray[i];
+    send(socket_id, & characters, sizeof(char), 0);
+  }
+
+  for (i = 0; i < 1; i++) {
+    characters = strlen(theArray) + '0';
+    send(socket_id, & characters, sizeof(char), 0);
+  }
+}
+void Send_Array_Data(int socket_id, char* array) {
+
+	int i=0;
+	/* Create an array of squares of first 30 whole numbers */
+	int simpleArray[ARRAY_SIZE] = {0};
+	for (i = 0; i < ARRAY_SIZE; i++) {
+		simpleArray[i] = i * i;
+	}
+
+	uint16_t statistics;  
+	for (i = 0; i < ARRAY_SIZE; i++) {
+		statistics = htons(simpleArray[i]);
+		send(socket_id, &statistics, sizeof(uint16_t), 0);
+	}
+}
+
+
+void *client_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+	puts("thread handling");
+    int sock = *(int*)socket_desc;
+    int read_size;
+    char *message , client_message[2000], user[15];
+	char* resultss = Receive_Array_char_Data(sock, 30);
+	for(int i = 0 ; i< 30; i++){
+	printf("array[%d] %c\n",i, resultss[i]);
+	}
+	char userEnteredName[15];
+      char arrayLim = resultss[28];
+      char arrayLim2 = resultss[29];
+      int intval = arrayLim - '0';
+      int intval2 = arrayLim2 - '0';
+      char pinEntered[intval2];
+	int match = 0;
+
+      //STORING PIN SENT TO SERVER FROM CLIENT
+      for (int i = 0; i < intval2; i++) {
+        pinEntered[i] = resultss[i + 15];
+      }
+      //STORING USERNAME SENT TO SERVER FROM CLIENT
+      for (int i = 0; i < intval; i++) {
+        userEnteredName[i] = resultss[i];
+      }
+      //CONVERT PIN FROM CHAR ARRAY TO INT VALUE
+      int sentPin;
+      sscanf(pinEntered, "%d", & sentPin);
+      printf("Pin Send :   %i\n\n ", sentPin);
+
+      //CHECKING WHATS GOING ON
+      printf("%c\n\n", arrayLim);
+      printf("%c\n\n", arrayLim2);
+      printf("%s\n\n", pinEntered);
+      printf("%i\n\n", intval);
+	int tracker = 0;
+
+	
+	 for (int i = 1; i < 11; i++) {
+        match = 0;
+        for (int k = 0; k < intval; k++) {
+          printf("%c  %c", clients[i].username[k], userEnteredName[k]);
+
+          //CONFIRMING USERNAME MATCHES
+          if (clients[i].username[k] == userEnteredName[k] && clients[i].pinNumber == sentPin) {
+            match = match + 1;
+          }
+        }
+	   if (match == intval) { //Bunch of character arrays to send to client
+					login = true;
+          printf("MATCH\n");
+
+          //Users client number
+          usersClientNum = clients[i].clientNumber;
+          for (int i = 0; i < 10; i++) {
+            //matching users client number against client details struct
+            //converting acc nums to char arrays
+            if (usersClientNum == clientsinfo[i].clientnum) {
+              sprintf(onlineCnum, "%d", usersClientNum);
+              sprintf(onlineAc1, "%d", clientsinfo[i].account1);
+              sprintf(onlineAc2, "%d", clientsinfo[i].account2);
+              sprintf(onlineAc3, "%d", clientsinfo[i].account3);
+              //name & surname char arrays
+              for (int k = 0; k < 15; k++) {
+                onlineName[k] = clientsinfo[i].name[k];
+                onlineSurname[k] = clientsinfo[i].surname[k];
+              }
+						for(int j = 0; j < 24; j++){
+						if(clientsinfo[tracker].account1 == accounts[j].accNum){
+						saveTracker = tracker;
+						clientsinfo[tracker].figs.open = accounts[j].openBal;
+						clientsinfo[tracker].figs.close = accounts[j].closeBal;
+						printf("\n\nClient Number = %i \n Account Number %i \n Opening Balance = %f \n Closing Balance = %f \n",clientsinfo[tracker].clientnum,clientsinfo[tracker].account1, clientsinfo[tracker].figs.open, clientsinfo[tracker].figs.close);
+}
+}
+
+										sprintf(onlineOpen, "%.2lf", clientsinfo[tracker].figs.open);
+						sprintf(onlineClose, "%.2lf", clientsinfo[tracker].figs.close);
+            }
+
+tracker++;
+          }
+					int ffs = strlen(onlineClose);
+					onlineClose[ffs] = '\0';
+					printf("\n%d", ffs);
+          for (int p = 0; p < 15; p++) {
+            //printf("%c\n", onlineAc1[p]);
+						printf("%c", onlineClose[p]);
+          }
+          printf("\nStored client num = %d", usersClientNum);
+          sendStrings(sock, onlineName, 14);
+          sendStrings(sock, onlineSurname, 14);
+          sendStrings(sock, onlineCnum, 14);
+          sendStrings(sock, onlineAc1, 14);
+          sendStrings(sock, onlineAc2, 14);
+          sendStrings(sock, onlineAc3, 14);
+	  sendStrings(sock, onlineOpen, 14);
+	  sendStrings(sock, onlineClose,14);
+
+        }
+        //Using store client number find user details
+        //variables for current user
+
+
+
+        printf("\n Username : %s\n Pin : %d\n Client Num : %d \n\n", clients[i].username, clients[i].pinNumber, clients[i].clientNumber);
+
+      } //  END FOR LOOP
+  //IF LOGIN = FALSE , CLOSE CONNECTION SEND MESSAGE
+      if (login == false) {
+        char incorrect[] = "ERROR Username and/or pin was INCORRECT";
+        send(sock, incorrect, strlen(incorrect) +1, 0);
+        close(sock);
+      }
+	
+
+	if(login == true){
+	
+	}
+
+
+
+    //Receive a message from client
+    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+    {
+	char username[1000], pin[1000],buf[1000];
+        //Send the message back to client
+	sscanf(client_message, "%s %s",username,pin);
+	printf("Client input %s, First word %s, Second word %s\n", client_message, username, pin);
+	if(strcmp(username, "BALANCE") == 0 && strcmp(pin, "SAVINGS") == 0) {
+	sprintf(buf, "\n\n %s %s Your balance for account %s is  $%s", onlineName, onlineSurname, onlineAc1 , onlineClose);
+
+	 write(sock , buf , strlen(buf));////////////////////////////
+	
+}
+
+	if(strcmp(username, "WITHDRAWSAV") == 0) {
+	clientsinfo[saveTracker].figs.close = clientsinfo[saveTracker].figs.close - atoi(pin);
+	sprintf(onlineClose, "%.2lf", clientsinfo[saveTracker].figs.close);
+	sprintf(buf, "\n\n %s %s Your balance for account %s is now $%s", onlineName, onlineSurname, onlineAc1 , onlineClose);
+	
+	 write(sock , buf , strlen(buf));////////////////////////////
+	
+}
+
+else{
+	 write(sock , "" , strlen("")+1);}
+       // write(sock , client_message , strlen(client_message)+1); }
+	//memset(client_message,"\n",sizeof(client_message));
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+         
+    //Free the socket pointer
+    free(socket_desc);
+     
+    return 0;
+}
+int main(int argc, char *argv[]) {
+
+	/* Thread and thread attributes */
+	pthread_t client_thread;
+	pthread_attr_t attr;
+
+
+	int sockfd, new_fd, *newestSock;  /* listen on sock_fd, new connection on new_fd */
+	struct sockaddr_in my_addr;    /* my address information */
+	struct sockaddr_in their_addr; /* connector's address information */
+	socklen_t sin_size;
+	int i=0;
+
+	/* Get port number for server to listen on */
+	if (argc != 2) {
+		fprintf(stderr,"usage: client port_number\n");
+		exit(1);
+	}
+
+	/* generate the socket */
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket");
+		exit(1);
+	}
+
+	/* generate the end point */
+	my_addr.sin_family = AF_INET;         /* host byte order */
+	my_addr.sin_port = htons(atoi(argv[1]));     /* short, network byte order */
+	my_addr.sin_addr.s_addr = INADDR_ANY; /* auto-fill with my IP */
+		/* bzero(&(my_addr.sin_zero), 8);   ZJL*/     /* zero the rest of the struct */
+
+	/* bind the socket to the end point */
+	if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) \
+	== -1) {
+		perror("bind");
+		exit(1);
+	}
+
+	/* start listnening */
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	printf("server starts listnening ...\n");
+	
+	//Fills the clients structre , username , pin , clientNum
+	fillClients();
+	//Fills the clients Info structure
+	fillClientsInfo();
+	//Fills the accounts structure
+	fillAccounts();
+
+	/* repeat: accept, send, close the connection */
+	/* for every accepted connection, use a sepetate process or thread to serve it */
+	while(1) {  /* main accept() loop */
+		sin_size = sizeof(struct sockaddr_in);
+		if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, \
+		&sin_size)) == -1) {
+			perror("accept");
+			continue;
+		}
+		printf("server: got connection from %s\n", \
+			inet_ntoa(their_addr.sin_addr));
+		newestSock = malloc(1);
+		*newestSock = new_fd;
+		
+
+		//Create a thread to accept client
+				
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&client_thread, &attr, client_handler,(void*) newestSock);
+
+		//pthread_join(client_thread,NULL);
+
+		//if (send(new_fd, "All of array data sent by server\n", 40 , 0) == -1)
+		//		perror("send");
+	if(new_fd < 0){
+	return 1;
+	}
+
+	}
+	return 0;
+ 
+}
+
